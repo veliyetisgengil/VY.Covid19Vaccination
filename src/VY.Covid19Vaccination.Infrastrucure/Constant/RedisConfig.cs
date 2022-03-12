ec4370cade5455df10514479ac17ccf0848a0871
Veli Yetisgengil <yetisgengil_veli@hotmail.com>
2022-03-12 19:57:44 +0300

    Update RedisConfig.cs

--- src/VY.Covid19Vaccination.Infrastrucure/Constant/RedisConfig.cs
+++ src/VY.Covid19Vaccination.Infrastrucure/Constant/RedisConfig.cs
@@ -3,9 +3,9 @@ namespace VY.Covid19Vaccination.Infrastructure.Const
 {
     public static class RedisConfig
     {
-        public const string Host = "covid19Turkey.redis.cache.windows.net:6380";
+        public const string Host = "*******";
         public const bool Ssl = true;
         public const bool AbortConnect = false;
-        public const string Password = "Zn2omOHtiqzroiz7t9CszRHcLL2LgqKenAzCaFXhs1U=";
+        public const string Password = "*************";
     }
 }
